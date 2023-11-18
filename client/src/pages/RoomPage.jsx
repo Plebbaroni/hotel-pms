10807e92d2fefaa5cb6971ee260f0362826cd419
lqo0opl <angelopumar987@gmail.com>
2023-11-18 18:33:22 +0800

    my changes

--- client/src/pages/RoomPage.jsx
+++ client/src/pages/RoomPage.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
+import "../css/RoomPage.css"
 
-function RoomPage({RoomType, RoomDescription}) {
-  const params = useParams();
+function RoomPage(props) {
+  const params = useParams(props);
+  console.log(params);
   
-
-
   return (
-    <div>{params.RoomType}</div>
+    <div className='RoomPage'>
+      <div className='RoomImage'>
+        <center>
+          <img src="" alt="" width={"90%"} height={"500"}/>
+          <br />
+          <p>Size of Occupancy, Amount of Beds, Amenities</p>
+          <button>Book Now</button>
+        </center>
+      </div>
+      <div className='RoomDescription'>
+        <center>
+            <h1>{params.roomType}</h1>
+            <hr style={{width: `90%`}}/>
+            <p>desct</p>
+        </center>
+      </div>
+    </div>
   )
 }
 
