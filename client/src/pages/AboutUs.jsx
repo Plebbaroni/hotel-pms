ac65eea559ee74cf19a98c483928d5f5bc7be6df
Jared <gorillacyborgz@gmail.com>
2023-12-08 18:14:11 +0800

    Marlex about us

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
-import "../css/Login.css"
+import "../css/Aboutus.css"
 
 function AboutUs() {
     return (
       <div className='wrapper'>
               <div className='transWrapper'>
-                  <div className='loginWrapper'>
-                      <h1>About Us</h1>
+                <div className='AboutUsWrapper'>
+                  <div className='header'>
+                    <h1>About Us</h1>
                   </div>
+                  <div className='profile-box'>
+                      <div className='profile'>
+                        <img src='../src/assets/jared.jpg' className='profile-img'></img>
+                        <h5>Jared Schulz</h5>
+                        <h6>Student</h6>
+                        <h6>BS CS 2</h6>
+                      </div>
+                      <div className='profile'>
+                        <img src='../src/assets/angelo.png' className='profile-img'></img>
+                        <h5>Angelo Pumar</h5>
+                        <h6>Student</h6>
+                        <h6>BS CS 2</h6>
+                      </div>
+                      <div className='profile'>
+                        <img src='../src/assets/marlex.jpg' className='profile-img'></img>
+                        <h5>Marlex Manalili</h5>
+                        <h6>Student</h6>
+                        <h6>BS CS 2</h6>
+                      </div>
+                      <div className='profile'>
+                        <img src='../src/assets/sean.jpg' className='profile-img'></img>
+                        <h5>Sean Duran</h5>
+                        <h6>Student</h6>
+                        <h6>BS CS 2</h6>
+                      </div>
+                  </div>
+                </div>
               </div> 
       </div>
       )
